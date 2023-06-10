7fd28b9168352d4475ae9fa5b5bda2ac62e74834
ihsankarim <ihsanalkarim303@gmail.com>
2023-06-10 13:34:01 +0700

    FMS

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">@lang('quickadmin.qa_dashboard')</div>
+
+                <div class="panel-body">
+                    @lang('quickadmin.qa_dashboard_text')
+    <h2 style="text-align: center;">Selamat Datan, Dashboard</h2>
+    <h3 style="text-align: center;"> File Management System</h3>
+    <p style="text-align: center;">Solusi mudah menyimpan file tanpa ganggunan!</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
